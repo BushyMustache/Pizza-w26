6417614e72b50f3899db841b0e519f7395b98455
BushyMustache <205280148+BushyMustache@users.noreply.github.com>
2026-02-05 14:25:31 -0800

    Add routing

--- app.js
+++ app.js
@@ -10,9 +10,52 @@ const PORT = 3000;
 // Enable static file serving
 app.use(express.static('public'));
 
+// "Middleware" that allows express to read
+// form data and store it in req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Create a temporary array to store orders
+const orders = [];
+
 // Define our main root ('/')
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// Contact Route
+app.get('/contact-us', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+});
+
+// Confirmation route
+app.get('/thank-you', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// Submit order route
+app.post('/submit-order', (req, res) => {
+    
+    // Create a JSON object to store the order data
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings ? req.body.toppings : "none",
+        size: req.body.size,
+        comment: req.body.comment ? req.body.comment : "none",
+        timestamp: new Date()
+    };
+
+    // Add order object to orders array
+    orders.push(order);
+    
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// Admin route
+app.get('/admin', (req, res) => {
+    res.send(orders);
 });
 
 // Start server and listen on designative port
